5c4c35083596ec940e5ffa76bd1e7d806329ceaf
Hector Larrea <taller.mcmoto@gmail.com>
2025-07-03 22:18:43 +0000

    aa

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,13 +5,14 @@
   channel = "stable-24.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    pkgs.nodejs_20
-    pkgs.zulu
+    pkgs.nodejs
     pkgs.bun
-    pkgs.openssl_3_3
+    pkgs.openssl
   ];
   # Sets environment variables in the workspace
-  env = {};
+  env = {
+    DATABASE_URL="prisma+postgres://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiMDFKWjk0UVA5SzBQSzZLVzJROThTOE1aS1EiLCJ0ZW5hbnRfaWQiOiIyYjdjMjUxYjY4YWY5MWIxMjE0OWQyZGQzMTVjOTliOTY1NThjMzBmYjU5MWY1MzdmYjU4NzJlODI1Y2JhOTIxIiwiaW50ZXJuYWxfc2VjcmV0IjoiZmJjODUzM2QtOWZkNC00Yzg0LWIwZGUtMTFlZjk3ZGRiZjU3In0.MjuUfWACDD9PJ7oZMsDxsqPoJ2U4i7IgQaEY-h_bBBM";
+  };
   # This adds a file watcher to startup the firebase emulators. The emulators will only start if
   # a firebase.json file is written into the user's directory
   services.firebase.emulators = {
@@ -22,7 +23,7 @@
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      # "vscodevim.vim"
+       "Prisma.prisma"
     ];
     workspace = {
       onCreate = {
