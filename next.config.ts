4021d7a881dd5c72c84325748c754b0f472c7726
Hector Larrea <taller.mcmoto@gmail.com>
2025-07-03 23:38:40 +0000

    b

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,6 @@ const nextConfig: NextConfig = {
   // @BestPractice: Añadido para permitir peticiones desde el entorno de Firebase Studio.
   // @ts-ignore - Se ignora el error de tipado para allowedDevOrigins en versiones anteriores de Next.js
   experimental: {
-    allowedDevOrigins: ["https://9000-firebase-studio-1751569887540.cluster-duylic2g3fbzerqpzxxbw6helm.cloudworkstations.dev"],
   },
 
   typescript: {
